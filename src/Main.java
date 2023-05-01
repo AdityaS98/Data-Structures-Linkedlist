923a8d69438b775ec4e05379e679aed0192cb4aa
AdityaS98 <adityasharmalko1998@gmail.com>
2023-05-01 18:34:26 +0530

    Added : A welcome message for linkedlist program

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.printf(" The following problem contains problems related to Linkedlist");
+
+
+    }
+}
